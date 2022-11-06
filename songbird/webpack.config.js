35e762f0d5034f7649fffc9e16d7b0b367ceeea5
Sergey P <s.pansevi4@gmail.com>
2022-11-06 12:16:26 +0300

    feat: add node-modules and webpack.config

---
+++ songbird/webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [
+      new CssMinimizerPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+
+  return config;
+};
+
+const filename = ext => isDev
+  ? `[name].${ ext }`
+  : `[name].[hash].${ ext }`;
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimization: optimization(),
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    port: 8080,
+    hot: false,
+  },
+  devtool: isDev ? 'source-map' : false,
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|png|jpg|svg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(inl.svg)$/,
+        type: 'asset/source',
+      },
+      {
+        test: /\.(mp3|ogg)$/,
+        type: 'asset',
+      },
+    ],
+  },
+};
